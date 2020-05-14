4dfcfd4216ae55f8239457d7760f06252c416813
ontofractal <v@ontofractal.com>
2020-05-14 14:23:05 +0300

    add prettier config

--- prettier.config.js
+++ prettier.config.js
@@ -1,6 +1,7 @@
 module.exports = {
-  trailingComma: "es5",
-  tabWidth: 2,
-  semi: false,
-  singleQuote: false,
+    trailingComma: "es5",
+    tabWidth: 4,
+    semi: false,
+    singleQuote: false,
+    bracketSpacing: false,
 }
